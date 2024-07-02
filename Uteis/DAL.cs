e042947fd7193c05d43c7b69db678fba65b97dbf
lisiarosa <lisia.koch@gmail.com>
2024-07-01 22:01:24 -0300

    Adicionar arquivos de projeto.

---
+++ Uteis/DAL.cs
@@ -0,0 +1,20 @@
+﻿namespace SistemaVendas.Uteis;
+using MySql.Data.MySqlClient;
+
+public class DAL
+{
+    private static string Server = "localhost";
+    private static string Database = "sistema_venda";
+    private static string User = "root";
+    private static string Password = "";
+    private static string ConnectionString = $"Server={Server};Database={Database};Uis={User};Pwd={Password};Sslmode=none;Charset=utf8;";
+    private static MySqlConnection Connection;
+
+    public DAL()
+    {
+        Connection = new MySqlConnection(ConnectionString);
+        Connection.Open();
+    }
+
+
+ }
