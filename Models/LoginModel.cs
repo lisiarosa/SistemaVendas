f6f92948677387bd5f52a443d25282ac78d00cda
lisiarosa <lisia.koch@gmail.com>
2024-07-05 23:19:47 -0300

    alguns ajustes no login

--- Models/LoginModel.cs
+++ Models/LoginModel.cs
@@ -1,11 +1,17 @@
 ﻿using SistemaVendas.Uteis;
 using System.Data;
+using System.ComponentModel.DataAnnotations;
 
 namespace SistemaVendas.Models
 {
     public class LoginModel
     {
+        [Required(ErrorMessage="Informe o e-mail do usuário!")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage ="O e-mail informado é inválido!")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Informe a senha do usuário!")]
         public string Senha { get; set; }
         public bool ValidarLogin()
         {
